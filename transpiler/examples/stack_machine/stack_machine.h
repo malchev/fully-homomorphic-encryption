55b1e42588445cde2bfe9ce038a42a988e2e9837
Iliyan Malchev <malchev@gmail.com>
2021-07-26 00:00:07 +0300

    experiment: refactor into lots of functions
    Maybe this can help the transpiler?
    
    Signed-off-by: Iliyan Malchev <malchev@gmail.com>

--- transpiler/examples/stack_machine/stack_machine.h
+++ transpiler/examples/stack_machine/stack_machine.h
@@ -28,26 +28,31 @@
 #define STATUS_MAX_CALLS             7
 
 // Opcodes
-#define OP_CALL '@' // push next instruction address to callstack, jump to argument
-#define OP_RET  '.' // pop op1 from callstack, continue at op1
-#define	OP_ADD  '+' // pop op1, pop op2, push op1 + op2
-#define	OP_SUB  '-' // pop op1, pop op2, push op2 - op1
-#define	OP_MUL  '*' // pop op1, pop op2, push op1 * op2
-#define OP_PUSH 'P' // push arg on top of stack
-#define OP_POP  'p' // pop top of stack
-#define OP_J    'j' // jump to arg location
-#define OP_JZ   'Z' // pop op1, jump to arg location if op1 == 0
-#define OP_JNZ  'z' // pop op1, jump to arg location if op1 != 0
-#define OP_DUP  'd' // duplicate the top of the stack
-#define OP_SWAP 'x' // swap the two topmost stack elements
-#define OP_EQ   '=' // consumes the top two operands and pushes 1 if they are =, 0 otherwise
-#define OP_NOT  '!' // consumes the top operand and pushes !op on the stack
-#define OP_AND  'A' // pop op1, pop op2, push op1 && op2
-#define OP_OR   'O' // pop op1, pop op2, push op1 || op2
-#define OP_BNOT '~' // consumes the top operand and pushes a bitwise not
-#define OP_BAND '&' // pop op1, pop op2, push (bitwise) op1 and op2
-#define OP_BOR  '|' // pop op1, pop op2, push (bitwise) op1 or op2
-#define OP_BXOR '^' // pop op1, pop op2, push (bitwise) op1 xor op2
+#define OP_CALL  0 // push next instruction address to callstack, jump to argument
+#define OP_RET   1 // pop op1 from callstack, continue at op1
+#define OP_PUSH  2 // push arg on top of stack
+#define OP_POP   3 // pop top of stack
+#define OP_J     4 // jump to arg location
+#define OP_JZ    5 // pop op1, jump to arg location if op1 == 0
+#define OP_JNZ   6 // pop op1, jump to arg location if op1 != 0
+#define OP_DUP   7 // duplicate the top of the stack
+#define OP_SWAP  8 // swap the two topmost stack elements
+#define OP_NOT   9 // consumes the top operand and pushes !op on the stack
+#define OP_BNOT 10 // consumes the top operand and pushes a bitwise not
+
+#define BINARY_OP_BASE 11
+
+#define OP_EQ   11 // consumes the top two operands and pushes 1 if they are =, 0 otherwise
+#define	OP_ADD  12 // pop op1, pop op2, push op1 + op2
+#define	OP_SUB  13 // pop op1, pop op2, push op2 - op1
+#define	OP_MUL  14 // pop op1, pop op2, push op1 * op2
+#define OP_AND  15 // pop op1, pop op2, push op1 && op2
+#define OP_OR   16 // pop op1, pop op2, push op1 || op2
+#define OP_BAND 17 // pop op1, pop op2, push (bitwise) op1 and op2
+#define OP_BOR  18 // pop op1, pop op2, push (bitwise) op1 or op2
+#define OP_BXOR 19 // pop op1, pop op2, push (bitwise) op1 xor op2
+
+#define MAX_OP  20
 
 // Max depth of operand stack
 #define MAX_OPD 1024
@@ -55,10 +60,14 @@
 #define MAX_CMD 1024
 // Maximum number of nested function call
 #define MAX_CALLS 999
+
+struct StackMachine;
+typedef int (*stack_machine_handler_t)(StackMachine &calc, int &result);
+typedef int (*handle_binary_op_t)(int op1, int op2);
 	
 struct StackMachine {
 
-	StackMachine() : top(0), call(0), step(0) {}
+	StackMachine();
 
 	int commands[MAX_CMD];
 	int stack[MAX_OPD];
@@ -67,6 +76,8 @@ struct StackMachine {
 	int call;
 	int step;
 
+	static stack_machine_handler_t dispatch[BINARY_OP_BASE];
+	static handle_binary_op_t binary_dispatch[MAX_OP - BINARY_OP_BASE];
 	static int tick(StackMachine &calc, int &result);
 	static int compute(StackMachine &calc, int max_gas, int &result);
 };
@@ -88,6 +99,222 @@ struct StackMachine {
 #define PRINT(...) do {} while(false)
 #endif
 
+StackMachine::StackMachine() 
+	: top(0), call(0), step(0) {
+}
+
+int handle_call(StackMachine &calc, int &result) {
+	if (calc.step == MAX_CMD) {
+		return STATUS_MAX_COMMANDS;
+	}
+	if (calc.call == MAX_CALLS) {
+		return STATUS_MAX_CALLS;
+	}
+	int target = calc.commands[calc.step];
+	calc.step++;
+	calc.callstack[calc.call] = calc.step;
+	calc.call++;
+	calc.step = target;
+	return STATUS_OK;
+}
+
+int handle_ret(StackMachine &calc, int &result) {
+	if (calc.call == 0) {
+		if (calc.top == 0) {
+			return STATUS_EMPTY_STACK;
+		}
+		calc.top--;
+		result = calc.stack[calc.top];
+		return STATUS_HALT;
+	}
+	else {
+		--calc.call;
+		calc.step = calc.callstack[calc.call];
+	}
+	return STATUS_OK;
+}
+
+int handle_push(StackMachine &calc, int &result) {
+	if (calc.step == MAX_CMD) {
+		return STATUS_MAX_COMMANDS;
+	}
+	if (calc.top == MAX_OPD) {
+		return STATUS_EXCEEDED_MAX_OPERANDS;
+	}
+	int val = calc.commands[calc.step];
+	calc.step++;
+	calc.stack[calc.top] = val;
+	calc.top++;
+	return STATUS_OK;
+}
+
+int handle_pop(StackMachine &calc, int &result) {
+	if (calc.top == 0) {
+		return STATUS_EMPTY_STACK;
+	}
+	--calc.top;
+	return STATUS_OK;
+}
+
+int handle_dup(StackMachine &calc, int &result) {
+	if (calc.top == 0) {
+		return STATUS_EMPTY_STACK;
+	}
+	int top = calc.stack[calc.top-1];
+	calc.stack[calc.top] = top;
+	calc.top++;
+	return STATUS_OK;
+}
+
+int handle_j(StackMachine &calc, int &result) {
+	if (calc.step == MAX_CMD) {
+		return STATUS_MAX_COMMANDS;
+	}
+	calc.step = calc.commands[calc.step];
+	return STATUS_OK;
+}
+
+int handle_j_cond(StackMachine &calc, int &result, bool want_zero) {
+	if (calc.step == MAX_CMD) {
+		return STATUS_MAX_COMMANDS;
+	}
+	if (calc.top == 0) {
+		return STATUS_EMPTY_STACK;
+	}
+	int target = calc.commands[calc.step];
+	calc.step++;
+	--calc.top;
+	bool is_zero = !calc.stack[calc.top];
+	if (!(want_zero ^ is_zero)) {
+		calc.step = target;
+	}
+	return STATUS_OK;
+}
+
+int handle_jz(StackMachine &calc, int &result) {
+	return handle_j_cond(calc, result, true);
+}
+
+int handle_jnz(StackMachine &calc, int &result) {
+	return handle_j_cond(calc, result, false);
+}
+
+int handle_not(StackMachine &calc, int &result) {
+	if (calc.top < 1) {
+		return STATUS_NOT_ENOUGH_OPERANDS;
+	}
+	--calc.top;
+	int op1 = calc.stack[calc.top];
+	calc.stack[calc.top] = !op1;
+	calc.top++;
+	return STATUS_OK;
+}
+
+int handle_bnot(StackMachine &calc, int &result) {
+	if (calc.top < 1) {
+		return STATUS_NOT_ENOUGH_OPERANDS;
+	}
+	--calc.top;
+	int op1 = calc.stack[calc.top];
+	calc.stack[calc.top] = ~op1;
+	calc.top++;
+	return STATUS_OK;
+}
+
+int handle_swap(StackMachine &calc, int &result) {
+	if (calc.top < 2) {
+		return STATUS_NOT_ENOUGH_OPERANDS;
+	}
+	--calc.top;
+	int ontop = calc.stack[calc.top];
+	--calc.top;
+	int below = calc.stack[calc.top];
+	calc.stack[calc.top] = ontop;
+	calc.top++;
+	calc.stack[calc.top] = below;
+	calc.top++;
+	return STATUS_OK;
+}
+
+int do_add(int op1, int op2) { return op1 + op2; }
+int do_sub(int op1, int op2) { return op2 - op1; }
+int do_mul(int op1, int op2) { return op1 * op2; }
+int do_and(int op1, int op2) { return op1 && op2; }
+int do_or(int op1, int op2) { return op1 || op2; }
+int do_band(int op1, int op2) { return op1 & op2; }
+int do_bor(int op1, int op2) { return op1 | op2; }
+int do_bxor(int op1, int op2) { return op1 ^ op2; }
+int do_eq(int op1, int op2) { return op1 == op2; }
+
+#if 1 // only trivial initializers are supported
+handle_binary_op_t StackMachine::binary_dispatch[MAX_OP - BINARY_OP_BASE] = {
+	do_eq,
+	do_add,
+	do_sub,
+	do_mul,
+	do_and,
+	do_or,
+	do_band,
+	do_bor,
+	do_bxor,
+};
+#else
+handle_binary_op_t StackMachine::binary_dispatch[MAX_OP - BINARY_OP_BASE] = {
+	[OP_ADD  - BINARY_OP_BASE] = do_add,
+	[OP_SUB  - BINARY_OP_BASE] = do_sub,
+	[OP_MUL  - BINARY_OP_BASE] = do_mul,
+	[OP_AND  - BINARY_OP_BASE] = do_and,
+	[OP_OR   - BINARY_OP_BASE] = do_or,
+	[OP_BAND - BINARY_OP_BASE] = do_band,
+	[OP_BOR  - BINARY_OP_BASE] = do_bor,
+	[OP_BXOR - BINARY_OP_BASE] = do_bxor,
+	[OP_EQ   - BINARY_OP_BASE] = do_eq,
+};
+#endif
+
+int handle_binary_op(StackMachine &calc, int &result, int op) {
+	if (calc.top < 2) {
+		return STATUS_NOT_ENOUGH_OPERANDS;
+	}
+	--calc.top;
+	int op1 = calc.stack[calc.top];
+	--calc.top;
+	int op2 = calc.stack[calc.top];
+	calc.stack[calc.top] = StackMachine::binary_dispatch[op](op1, op2);
+	calc.top++;
+	return STATUS_OK;
+}
+
+#if 1 // only trivial initializers are supported
+stack_machine_handler_t StackMachine::dispatch[BINARY_OP_BASE] = {
+	handle_call, 
+	handle_ret, 
+	handle_push, 
+	handle_pop, 
+	handle_j, 
+	handle_jz, 
+	handle_jnz, 
+	handle_dup, 
+	handle_swap, 
+	handle_not, 
+	handle_bnot, 
+};
+#else
+stack_machine_handler_t StackMachine::dispatch[BINARY_OP_BASE] = {
+	[OP_CALL]  = handle_call, 
+	[OP_RET]   = handle_ret, 
+	[OP_PUSH]  = handle_push, 
+	[OP_POP]   = handle_pop, 
+	[OP_J]     = handle_j, 
+	[OP_JZ]    = handle_jz, 
+	[OP_JNZ]   = handle_jnz, 
+	[OP_DUP]   = handle_dup, 
+	[OP_SWAP]  = handle_swap, 
+	[OP_NOT]   = handle_not, 
+	[OP_BNOT]  = handle_bnot, 
+};
+#endif
+
 int StackMachine::tick(StackMachine &calc, int &result) {
 #ifdef DEBUG
 	do {
@@ -104,231 +331,17 @@ int StackMachine::tick(StackMachine &calc, int &result) {
 	}
 	int cmd = calc.commands[calc.step];
 	calc.step++;
-	if (cmd == OP_CALL) {
-		if (calc.step == MAX_CMD) {
-			return STATUS_MAX_COMMANDS;
-		}
-		if (calc.call == MAX_CALLS) {
-			return STATUS_MAX_CALLS;
-		}
-		int target = calc.commands[calc.step];
-		calc.step++;
-		calc.callstack[calc.call] = calc.step;
-		calc.call++;
-		calc.step = target;
-	}
-	else if (cmd == OP_RET) {
-		if (calc.call == 0) {
-			if (calc.top == 0) {
-				return STATUS_EMPTY_STACK;
-			}
-			calc.top--;
-			result = calc.stack[calc.top];
-			return STATUS_HALT;
-		}
-		else {
-			--calc.call;
-			calc.step = calc.callstack[calc.call];
-		}
-	}
-	else if (cmd == OP_PUSH) {
-		if (calc.step == MAX_CMD) {
-			return STATUS_MAX_COMMANDS;
-		}
-		if (calc.top == MAX_OPD) {
-			return STATUS_EXCEEDED_MAX_OPERANDS;
-		}
-		int val = calc.commands[calc.step];
-		calc.step++;
-		calc.stack[calc.top] = val;
-		calc.top++;
-	}
-	else if (cmd == OP_POP) {
-		if (calc.top == 0) {
-			return STATUS_EMPTY_STACK;
-		}
-		--calc.top;
-	}
-	else if (cmd == OP_DUP) {
-		if (calc.top == 0) {
-			return STATUS_EMPTY_STACK;
-		}
-		int top = calc.stack[calc.top-1];
-		calc.stack[calc.top] = top;
-		calc.top++;
-	}
-	else if (cmd == OP_J) {
-		if (calc.step == MAX_CMD) {
-			return STATUS_MAX_COMMANDS;
-		}
-		calc.step = calc.commands[calc.step];
-	}
-	else if (cmd == OP_JZ) {
-		if (calc.step == MAX_CMD) {
-			return STATUS_MAX_COMMANDS;
-		}
-		if (calc.top == 0) {
-			return STATUS_EMPTY_STACK;
-		}
-		int target = calc.commands[calc.step];
-		calc.step++;
-		--calc.top;
-		int top = calc.stack[calc.top];
-		if (!top) {
-			calc.step = target;
-		}
-	}
-	else if (cmd == OP_JNZ) {
-		if (calc.step == MAX_CMD) {
-			return STATUS_MAX_COMMANDS;
-		}
-		if (calc.top == 0) {
-			return STATUS_EMPTY_STACK;
-		}
-		int target = calc.commands[calc.step];
-		calc.step++;
-		--calc.top;
-		int top = calc.stack[calc.top];
-		if (top) {
-			calc.step = target;
-		}
-	}
-	else if (cmd == OP_NOT) {
-		if (calc.top < 1) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		calc.stack[calc.top] = !op1;
-		calc.top++;
-	}
-	else if (cmd == OP_AND) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 && op2;
-		calc.top++;
-	}
-	else if (cmd == OP_OR) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 || op2;
-		calc.top++;
-	}
-	else if (cmd == OP_BNOT) {
-		if (calc.top < 1) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		calc.stack[calc.top] = ~op1;
-		calc.top++;
-	}
-	else if (cmd == OP_BAND) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 & op2;
-		calc.top++;
-	}
-	else if (cmd == OP_BOR) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 | op2;
-		calc.top++;
-	}
-	else if (cmd == OP_BXOR) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 ^ op2;
-		calc.top++;
-	}
 
-	else if (cmd == OP_SWAP) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int ontop = calc.stack[calc.top];
-		--calc.top;
-		int below = calc.stack[calc.top];
-		calc.stack[calc.top] = ontop;
-		calc.top++;
-		calc.stack[calc.top] = below;
-		calc.top++;
-	}
-	else if (cmd == OP_EQ) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 == op2;
-		calc.top++;
-	}
-	else if (cmd == OP_ADD) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 + op2;
-		calc.top++;
-	}
-	else if (cmd == OP_SUB) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 - op2;
-		calc.top++;
-	}
-	else if (cmd == OP_MUL) {
-		if (calc.top < 2) {
-			return STATUS_NOT_ENOUGH_OPERANDS;
-		}
-		--calc.top;
-		int op2 = calc.stack[calc.top];
-		--calc.top;
-		int op1 = calc.stack[calc.top];
-		calc.stack[calc.top] = op1 * op2;
-		calc.top++;
-	}
-	else {
+	if (cmd < 0) {
 		return STATUS_UNKNOWN_OPCODE;
 	}
-
-	return STATUS_OK;
+	if (cmd < BINARY_OP_BASE) {
+		return StackMachine::dispatch[cmd](calc, result);
+	}
+	if (cmd < MAX_OP) {
+		return handle_binary_op(calc, result, cmd - BINARY_OP_BASE);
+	}
+	return STATUS_UNKNOWN_OPCODE;
 }
 
 int StackMachine::compute(StackMachine &calc, int max_gas, int &result) {
